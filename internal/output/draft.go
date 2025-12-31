1c43510692687d3a9cb40f90b200d118fbc10d33
Ricardo Siqueira <ricardo.siqueira@commure.com>
2025-12-31 00:05:29 -0300

    lithography: implementation...

--- internal/output/draft.go
+++ internal/output/draft.go
@@ -21,7 +21,6 @@ const (
 )
 
 func PrintDraft(resp *llm.Run, format OutputFormat) error {
-	// test comment
 	switch format {
 	case FormatPretty:
 		fmt.Printf("🆕 **PR Title:** %s\n\n📝 **Description:**\n%s\n\n💬 **Commit:** %s\n",
